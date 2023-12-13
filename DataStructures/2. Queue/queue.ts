dfc57b8bcc59cd6c777993b6c5fd5ff039735f46
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-12-13 23:47:42 +0100

    Update queue.ts

--- DataStructures/2. Queue/queue.ts
+++ DataStructures/2. Queue/queue.ts
@@ -1,6 +1,5 @@
 class Queuez<T> {
     private queue: Array<T> = []
-    private front: number = 0
     private back: number = 0
     private MAX: number = 1000
     private count = 0;
@@ -26,14 +25,12 @@ class Queuez<T> {
         return "Nothing to dequeue"
   }
   peek() {
-    return this.count === 0 ? undefined : this.queue[this.front];
+    return this.count === 0 ? undefined : this.queue[0];
   }
   size() {
     return this.count;
   }
   isEmpty() {
-    this.front = 0
-    this.back == 0
     return this.count === 0;
   }
 }
