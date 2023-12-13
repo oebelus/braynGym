2b7d084e43a6813fdc4c961ed34b4ce122c3e82f
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2023-12-13 23:42:23 +0100

    Update queue.ts

--- DataStructures/2. Queue/queue.ts
+++ DataStructures/2. Queue/queue.ts
@@ -1,23 +1,17 @@
-class Queue<T> {
-    private queue: Array<T>;
-    private front: number;
-    private back: number;
-    private MAX: number = 1000;
+class Queuez<T> {
+    private queue: Array<T> = []
+    private front: number = 0
+    private back: number = 0
+    private MAX: number = 1000
     private count = 0;
     
-  constructor() {
-    this.queue = [];
-    this.front = 0;
-    this.back = 0;
-  }
   print() {
     console.log(this.queue);
   }
   enqueue(val:T) {
     if (this.count < this.MAX) {
-        this.queue[this.back] = val;
-        this.back = (this.back + 1) % this.MAX;
-        this.count++;
+        this.queue[this.back++] = val;
+        this.count++
     }
     else {
         throw new Error("Out of Memory")
@@ -25,13 +19,11 @@ class Queue<T> {
   }
   dequeue() {
     if (this.count > 0) {
-        let first:T = this.queue[this.front];
-        this.front = (this.front + 1) % this.MAX;
         this.count--;
-        return first;
-    } else {
-        return undefined;
+        return this.queue.shift()
     }
+    else
+        return "Nothing to dequeue"
   }
   peek() {
     return this.count === 0 ? undefined : this.queue[this.front];
@@ -40,6 +32,8 @@ class Queue<T> {
     return this.count;
   }
   isEmpty() {
+    this.front = 0
+    this.back == 0
     return this.count === 0;
   }
 }
