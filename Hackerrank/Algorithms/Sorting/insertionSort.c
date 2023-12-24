c76e8dc21409d763a4bd5a176ef73dbe256b9c1f
Imane <imaneelfidrissi@hotmail.com>
2023-12-25 00:12:57 +0100

    Update insertionSort.c

--- Hackerrank/Algorithms/Sorting/insertionSort.c
+++ Hackerrank/Algorithms/Sorting/insertionSort.c
@@ -14,3 +14,20 @@ void insertionSort(int N, int arr[]) {
         printf("%d ", arr[j]);
     }
 }
+
+
+int runningTime(int arr_count, int* arr) {
+    int count = 0;
+    for (int i = 1; i < arr_count; i++) {
+        int j = i;
+        int val = arr[i];
+        while (j > 0 && val < arr[j-1]) {
+                count++;
+                arr[j] = arr[j-1];
+                j--;
+        }
+        arr[j] = val;
+    }
+    return count;
+}
+
