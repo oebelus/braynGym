34abca6b5b7d79b531bd08595177944814aefa4f
Imane <imaneelfidrissi@hotmail.com>
2024-01-09 15:50:39 +0100

    Update hasPath.ts

--- Data Structures/6. Graph/hasPath.ts
+++ Data Structures/6. Graph/hasPath.ts
@@ -1,4 +1,5 @@
 function hasPath(graph: Record<string, (string|never)[]>, src: string, dst: string): boolean {
+  if (dst == src) return true
   const stack: string[] = [src]
   while (stack.length > 0) {
     const current = stack.pop()
