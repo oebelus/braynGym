8f41e09750e5ead433b78486ad5dd53021dca7a1
Imane <imaneelfidrissi@hotmail.com>
2024-01-09 15:47:45 +0100

    Create hasPath.ts

---
+++ Data Structures/6. Graph/hasPath.ts
@@ -0,0 +1,14 @@
+function hasPath(graph: Record<string, (string|never)[]>, src: string, dst: string): boolean {
+  const stack: string[] = [src]
+  while (stack.length > 0) {
+    const current = stack.pop()
+    console.log(current)
+    if (current) {
+      if (current == dst) return true
+      for (let neighbour of graph[current]) {
+        stack.push(neighbour)
+      }
+    }
+  }
+  return false
+}
