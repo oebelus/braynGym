ebf73343fc1810bd7deb6cc57514f62c5e38de5f
Imane <imaneelfidrissi@hotmail.com>
2024-01-09 14:39:49 +0100

    Create breadthFirstTrav.ts

---
+++ Data Structures/6. Graph/breadthFirstTrav.ts
@@ -0,0 +1,23 @@
+const graph: Record<string, (string|never)[]> = {
+  a: ['b', 'c'],
+  b: ['d'],
+  c: ['e'],
+  d: ['f'],
+  e: [],
+  f: []
+}
+
+function breadthFirstTrav(graph:Record<string, (string|never)[]>, source: string):void {
+  const queue = [source] // shift and push 
+  while (queue.length > 0) {
+    const current = queue.shift()
+    console.log(current)
+    if (current) {
+      for (let neighbour of graph[current]) {
+        queue.push(neighbour)
+      }
+    }
+  }
+}
+
+breadthFirstTrav(graph, 'a') // abcdef
