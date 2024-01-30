f0332b295df94194948ce66e9e034c5da901fdc8
Imane <imaneelfidrissi@hotmail.com>
2024-01-30 03:14:20 +0100

    Create mergeLists.c

---
+++ HackerRank/Data Structures/Linked List/mergeLists.c
@@ -0,0 +1,44 @@
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode* linked = (struct SinglyLinkedListNode*)malloc(sizeof(struct SinglyLinkedListNode));
+    SinglyLinkedListNode* current = linked; 
+    linked->data = 0;
+    linked->next = NULL;
+    
+    while (head1 && head2) {
+        SinglyLinkedListNode* add = (struct SinglyLinkedListNode*)malloc(sizeof(struct SinglyLinkedListNode));
+        add->data = 0;
+        add->next = NULL;
+        if (head1->data <= head2->data) {
+            add->data = head1->data;
+            current->next = add;
+            current = current->next;
+            head1 = head1->next;
+        } else if (head1->data >= head2->data) {
+            add->data = head2->data;
+            current->next = add;
+            current = current->next;
+            head2 = head2->next;
+        }
+    }
+    while (head1) {
+        SinglyLinkedListNode* add = (struct SinglyLinkedListNode*)malloc(sizeof(struct SinglyLinkedListNode));
+        add->data = 0;
+        add->next = NULL;
+        add->data = head1->data;
+        current->next = add;
+        current = current->next;
+        head1 = head1->next;
+    }
+    
+    while (head2) {
+        SinglyLinkedListNode* add = (struct SinglyLinkedListNode*)malloc(sizeof(struct SinglyLinkedListNode));
+        add->data = 0;
+        add->next = NULL;
+        add->data = head2->data;
+        current->next = add;
+        current = current->next;
+        head2 = head2->next;
+    }
+    current = linked->next;
+    return linked->next;
+}
