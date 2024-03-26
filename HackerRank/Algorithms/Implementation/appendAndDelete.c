8fbbd100c244de536dd25e28be08a5413038a4ae
Imane <imaneelfidrissi@hotmail.com>
2024-03-26 01:21:26 +0000

    Create appendAndDelete.c

---
+++ HackerRank/Algorithms/Implementation/appendAndDelete.c
@@ -0,0 +1,44 @@
+int length(char* s) {
+    int count = 0;
+    while (s[count]) {
+        count++;
+    }
+    return count;
+}
+
+char* appendAndDelete(char* s, char* t, int k) {
+    int lengthS = length(s);
+    int lengthT = length(t);
+    int count = 0;
+    int passed = 0;
+    if (lengthS > lengthT) {
+        count += lengthS - lengthT;
+        for (int i = 0; i < lengthS; i++) {
+            if (s[i] == t[i]) {
+                count += 0;
+            }
+            else {
+                count += lengthT - i;
+                while (t[i]) {
+                    count += 1;
+                    i++;
+                }
+                break;
+            }
+        }
+    } else if (lengthS == lengthT) {
+        int i = 0;
+        while (s[i] && s[i] == t[i]) {
+            count++;
+            i++;
+        }
+        if (count == lengthS) count = k;
+        else count = k >= lengthS + lengthT ? k : lengthS + lengthT;
+    } else {
+        count += lengthT - lengthS;
+        if (s[lengthS-1] == t[lengthT-1]) count = k;
+        else count = k + 1;
+    }
+    if (count <= k && count != 0) return "Yes";
+    else return "No";
+}
