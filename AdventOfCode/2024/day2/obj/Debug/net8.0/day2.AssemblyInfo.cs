e4e6ea07e806b1509ebe53b832135a973b1f3852
oebelus <imaneelfidrissi@hotmail.com>
2024-12-02 21:09:18 +0100

    day 2 advent of code

--- AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
+++ AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69cc2934925cf27d7a81cc9412c4623d3d49d99f")]
 [assembly: System.Reflection.AssemblyProductAttribute("day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
