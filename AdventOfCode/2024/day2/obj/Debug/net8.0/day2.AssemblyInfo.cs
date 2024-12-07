6a70d6a4b14400f09c12da0db5b7d75dfa73309f
oebelus <imaneelfidrissi@hotmail.com>
2024-12-07 19:03:22 +0100

    day 4 part 2

--- AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
+++ AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4e6ea07e806b1509ebe53b832135a973b1f3852")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+814c27b4aeaa6459c6bfac3c286e10ab05ceb734")]
 [assembly: System.Reflection.AssemblyProductAttribute("day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AdventOfCode/2024/day4/Program.cs
@@ -0,0 +1,101 @@
+﻿class Ceres
+{
+    public static List<List<char>> matrix = File.ReadLines("words.txt").ToList().Select(line => line.ToCharArray().ToList()).ToList();
+    public readonly static List<int[]> directions = [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]];
+
+    public readonly static List<int[]> diagonals = [[-1, -1], [-1, 1], [1, -1], [1, 1]];
+
+    private readonly static int rows = matrix.Count;
+    private readonly static int columns = matrix[0].Count;
+    private readonly static string toFind = "XMAS";
+
+    public static void Main()
+    {
+        Console.WriteLine(FindA());
+    }
+
+    // 1. First Challenge 
+
+    public static int FindOccurences()
+    {
+        int count = 0;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                if (matrix[i][j] == toFind[0])
+                {
+                    count += FindWord(i, j);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static int FindWord(int i, int j)
+    {
+        int occurences = 0;
+
+        foreach (var direction in directions)
+        {
+            int k = i;
+            int l = j;
+            int charIndex = 0;
+
+            while (charIndex < toFind.Length)
+            {
+                if (k < 0 || k >= rows || l < 0 || l >= columns ||
+                    matrix[k][l] != toFind[charIndex])
+                {
+                    break;
+                }
+
+                charIndex++;
+                k += direction[0];
+                l += direction[1];
+
+            }
+
+            if (charIndex == toFind.Length) occurences++;
+        }
+
+        return occurences;
+    }
+
+    // 2. Second Challenge (Incomplete)
+    public static int FindA()
+    {
+        int count = 0;
+
+        for (int i = 1; i < rows - 1; i++)
+        {
+            for (int j = 1; j < columns - 1; j++)
+            {
+                if (matrix[i][j] == 'A')
+                {
+                    count += FindCross(i, j) ? 1 : 0;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static bool FindCross(int i, int j)
+    {
+        char topLeft = matrix[i - 1][j - 1];
+        char topRight = matrix[i - 1][j + 1];
+        char bottomLeft = matrix[i + 1][j - 1];
+        char bottomRight = matrix[i + 1][j + 1];
+
+        if (((topLeft == 'M' && bottomRight == 'S') || (topLeft == 'S' && bottomRight == 'M')) &&
+            ((topRight == 'M' && bottomLeft == 'S') || (topRight == 'S' && bottomLeft == 'M')))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}
