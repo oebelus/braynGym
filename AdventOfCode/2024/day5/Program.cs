149ff43c002f9981b78acddf7918555941364d0f
oebelus <imaneelfidrissi@hotmail.com>
2024-12-08 13:43:35 +0100

    day 5 part 2

--- AdventOfCode/2024/day5/Program.cs
+++ AdventOfCode/2024/day5/Program.cs
@@ -4,9 +4,27 @@ class PrintQueue
 {
     private static Dictionary<int, List<int>> adjacency = [];
     private static List<List<int>> paths = [];
-    private static int count = 0;
+    private static int correct = 0;
+    private static int incorrect = 0;
 
     public static void Main()
+    {
+        ParseInput();
+
+        foreach (List<int> path in paths)
+        {
+            if (CheckPath(path)) correct += path[path.Count / 2];
+            else
+            {
+                incorrect += InsertionSort(path);
+            }
+        }
+
+        Console.WriteLine(correct);
+        Console.WriteLine(incorrect);
+    }
+
+    public static void ParseInput()
     {
         var lines = File.ReadLines("numbers.txt");
 
@@ -31,24 +49,6 @@ class PrintQueue
             }
         }
 
-        foreach (var (key, value) in adjacency)
-        {
-            Console.Write($"{key}: ");
-
-            foreach (var item in value)
-            {
-                Console.Write($"{item} ");
-            }
-
-            Console.WriteLine();
-        }
-
-        foreach (List<int> path in paths)
-        {
-            count += CheckPath(path) ? path[path.Count / 2] : 0;
-        }
-
-        Console.WriteLine(count);
     }
 
     // Part 1
@@ -65,5 +65,29 @@ class PrintQueue
         }
 
         return true;
+    }
+
+    // part 2
+    public static int InsertionSort(List<int> path)
+    {
+        int length = path.Count;
+
+        for (int i = 1; i < length; i++)
+        {
+            int key = path[i];
+            int j = i - 1;
+
+            if (!adjacency.ContainsKey(key)) continue;
+
+            while (j >= 0 && adjacency[key].Contains(path[j]))
+            {
+                path[j + 1] = path[j];
+                j -= 1;
+            }
+
+            path[j + 1] = key;
+        }
+
+        return path[length / 2];
     }
 }
