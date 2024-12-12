16e400208fbc0ba2396d6379ceb42992d42129db
oebelus <imaneelfidrissi@hotmail.com>
2024-12-12 13:36:25 +0100

    day 7 part 1

--- AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
+++ AdventOfCode/2024/day2/obj/Debug/net8.0/day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9ce0b1597da9f32562266d3167646dba0316b51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+629e01f5a1bf584fa7f2a241182ca0e7e0a3e8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AdventOfCode/2024/day7/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.Numerics;
+
+class BridgeRepair
+{
+    private static BigInteger count = 0;
+
+    public static void Main()
+    {
+        EvaluateExpression();
+    }
+
+    // Part 1
+    private static void EvaluateExpression()
+    {
+        var input = File.ReadAllLines("numbers.txt");
+
+        foreach (var line in input)
+        {
+            var arr = line.Split(':');
+
+            var calibration = new
+            {
+                Result = BigInteger.Parse(arr[0].Trim()),
+                Operands = arr[1].Trim().Split(' ')
+                    .Select(s => BigInteger.Parse(s.Trim()))
+                    .Reverse()
+                    .ToList()
+            };
+
+            var possibleResults = Evaluate(calibration.Operands, []);
+
+            if (possibleResults.Contains(calibration.Result))
+                count += calibration.Result;
+        }
+
+        Console.WriteLine(count);
+    }
+
+    private static List<BigInteger> Evaluate(List<BigInteger> operands, Dictionary<string, List<BigInteger>> cache)
+    {
+        string key = string.Join(".", operands);
+
+        if (cache.ContainsKey(key)) return cache[key];
+
+        List<BigInteger> result = [];
+
+        if (operands.Count == 2)
+        {
+            result.Add(operands[0] + operands[1]);
+            result.Add(operands[0] * operands[1]);
+        }
+        else
+        {
+            BigInteger left = operands[0];
+            List<BigInteger> rights = Evaluate(operands[1..], cache);
+
+            foreach (var right in rights)
+            {
+                foreach (var n in Evaluate([left, right], cache))
+                {
+                    result.Add(n);
+                }
+            }
+        }
+
+        cache.Add(key, result);
+
+        return result;
+    }
+}
