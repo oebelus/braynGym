7d3391ac6f4ec4a4389d7b187c11c0c4f6a9c1f1
oebelus <imaneelfidrissi@hotmail.com>
2024-12-16 20:20:59 +0100

    day 12 part 1

--- AdventOfCode/2024/day11/Program.cs
+++ AdventOfCode/2024/day11/Program.cs
@@ -1,12 +1,23 @@
-﻿class PlutonianPebbles
+﻿using System.Diagnostics;
+
+class PlutonianPebbles
 {
     public static List<long> Numbers = [];
-    private static readonly int blinks = 25;
+    private static readonly int blinks = 75;
+    private static readonly int blinksFrequency = 25;
 
     public static void Main()
     {
+        Stopwatch timer = Stopwatch.StartNew();
+
         ParseInput();
-        Console.WriteLine(Blink(0, Numbers));
+
+        int total = Blink(0, blinksFrequency, Numbers);
+
+        timer.Stop();
+
+        Console.WriteLine($"Total Stones: {total}");
+        Console.WriteLine($"Seconds: {timer.Elapsed.TotalSeconds}");
     }
 
     private static void ParseInput()
@@ -17,9 +28,9 @@
     }
 
     // Part 1
-    private static int Blink(int currentBlink, List<long> stones)
+    private static int Blink(int currentBlink, int maxBlinks, List<long> stones)
     {
-        if (currentBlink == blinks) return stones.Count;
+        if (currentBlink == maxBlinks) return stones.Count;
 
         int length = stones.Count;
 
@@ -41,6 +52,6 @@
 
             else buffer.Add(stones[i] * 2024);
         }
-        return Blink(currentBlink + 1, buffer);
+        return Blink(currentBlink + 1, maxBlinks, buffer);
     }
 }
---
+++ AdventOfCode/2024/day12/Program.cs
@@ -0,0 +1,117 @@
+﻿class GardenGroups
+{
+    private static List<List<char>> Fences = [];
+    private static List<string> VisitedFences = [];
+    private static int Rows = 0;
+    private static int Cols = 0;
+    private static int Stats = 0;
+
+    public static void Main()
+    {
+        ParseInput();
+
+        NumberOfFences();
+
+        Console.WriteLine(Stats);
+    }
+
+    private static void ParseInput()
+    {
+        var file = File.ReadAllLines("garden.txt");
+
+        Rows = file.Length;
+        Cols = file[0].Length;
+
+        for (int i = 0; i < Rows; i++)
+        {
+            Fences.Add([.. file[i].ToCharArray()]);
+        }
+    }
+
+    private static void NumberOfFences()
+    {
+        for (int i = 0; i < Rows; i++)
+        {
+            for (int j = 0; j < Cols; j++)
+            {
+                var coordinates = FloodFill(i, j, Fences[i][j], [], new bool[Rows, Cols]);
+
+                string key = "";
+
+                coordinates.Sort((a, b) =>
+                {
+                    if (a[0] == b[0])
+                        return a[1].CompareTo(b[1]);
+                    return a[0].CompareTo(b[0]);
+                });
+
+                foreach (var coordinate in coordinates)
+                {
+                    key += $"{coordinate[0]}-{coordinate[1]}";
+                }
+
+                if (!VisitedFences.Contains(key))
+                {
+                    VisitedFences.Add(key);
+
+                    int area = coordinates.Count;
+                    int perimeter = CalculatePerimeter(coordinates);
+
+                    Stats += area * perimeter;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+
+    private static List<List<int>> FloodFill(int row, int col, char flower, List<List<int>> coordinates, bool[,] visited)
+    {
+        if (row < 0 || row >= Rows || col < 0 || col >= Cols || visited[row, col] || Fences[row][col] != flower) return coordinates;
+
+        visited[row, col] = true;
+        coordinates.Add([row, col]);
+
+        // getting coordinates 
+        FloodFill(row - 1, col, flower, coordinates, visited);
+        FloodFill(row + 1, col, flower, coordinates, visited);
+        FloodFill(row, col - 1, flower, coordinates, visited);
+        FloodFill(row, col + 1, flower, coordinates, visited);
+
+        return coordinates;
+    }
+
+    private static int CalculatePerimeter(List<List<int>> coordinates)
+    {
+        int perimeter = 0;
+
+        HashSet<string> coordinateSet = [];
+
+        foreach (var coord in coordinates)
+        {
+            coordinateSet.Add($"{coord[0]}-{coord[1]}");
+        }
+
+        foreach (var coord in coordinates)
+        {
+            int row = coord[0];
+            int col = coord[1];
+
+            // If a neighbour is missing, it's an edge ;)
+            if (!coordinateSet.Contains($"{row - 1}-{col}")) perimeter++;
+            if (!coordinateSet.Contains($"{row + 1}-{col}")) perimeter++;
+            if (!coordinateSet.Contains($"{row}-{col - 1}")) perimeter++;
+            if (!coordinateSet.Contains($"{row}-{col + 1}")) perimeter++;
+        }
+
+        return perimeter;
+    }
+
+}
+
+/* 
+    area: number of plots
+    perimeter: number of sides that do not touch another garden plot in the same region
+*/
