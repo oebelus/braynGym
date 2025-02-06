92c07448a67353efe276974ed26f4c4bd6d08d1f
Oebelus <imaneelfidrissi@hotmail.com>
2025-02-06 07:13:49 +0100

    Update group_anagrams.rs

--- LeetCode/49. Group Anagrams/group_anagrams.rs
+++ LeetCode/49. Group Anagrams/group_anagrams.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
-pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+// Naive solution.
+pub fn group_anagrams_naive(strs: Vec<String>) -> Vec<Vec<String>> {
     let mut groups = HashMap::<String, Vec<String>>::new();
 
     for s in strs {
@@ -18,4 +19,28 @@ pub fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
     }
 
     groups.into_values().collect()
+}
+
+
+// Solution using an array of size O(26) to index the HashMap of groups of anagrams. 
+fn group_anagrams(strs: Vec<String>) -> Vec<Vec<String>> {
+    let mut result: HashMap<String, Vec<String>> = Default::default();
+
+    for s in strs {
+        let mut count = [0; 26];
+
+        for c in s.chars() {
+            count[(c as u8 - b'a') as usize] += 1;
+        }
+
+        let key = count
+            .iter()
+            .map(|&x| x.to_string())
+            .collect::<Vec<String>>()
+            .join(",");
+
+        result.entry(key).or_insert(Vec::new()).push(s);
+    }
+
+    return result.into_values().collect();
 }
